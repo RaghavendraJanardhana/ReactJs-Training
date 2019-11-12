66c817389fe80d5b9edd271eaf11c858513d5b28
Raghavendra J <Raghavendra.janardhana@tecnotree.com>
2019-11-12 15:52:12 +0530

    After the first static react page.

---
+++ first-project/src/components/Header.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class Header extends Component{
+    render(){
+        return(
+            <header>
+  <h2>Cities</h2>
+</header>
+        )
+    }
+}
+export default Header;
